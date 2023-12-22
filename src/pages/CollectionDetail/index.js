d3b0c3786150f314fe4c49270ed7ddbda7068e35
Muhammad-Zuhaib13 <muhammadzohaib578@gmail.com>
2023-12-22 10:32:36 +0500

    nft king template

---
+++ src/pages/CollectionDetail/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../../layout/Layout'
+import CollectionDetailSection from '../../components/CollectionDetails/CollectionDetailSection'
+
+const CollectionDetail = () => {
+  return (
+    <Layout>
+    <CollectionDetailSection/>
+    </Layout>
+  )
+}
+
+export default CollectionDetail
